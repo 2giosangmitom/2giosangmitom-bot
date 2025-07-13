8ec81bab44d8dc5a635d389cc6fe1df3b1189211
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-13 20:50:19 +0700

    feat(utils): add random utilities

---
+++ include/utils/random.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <random>
+
+namespace random_utils {
+
+// Generate a random number in range [a, b]
+template <typename T> T random_int_range(T a, T b) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(a, b);
+  return distrib(gen);
+};
+
+} // namespace random_utils
