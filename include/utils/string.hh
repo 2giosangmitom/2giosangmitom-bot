4ab756c475e45194048d443fe105a5277a279259
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-14 09:26:06 +0700

    feat(utils): add split string utils

--- include/utils/string.hh
+++ include/utils/string.hh
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace string_utils {
 
 // Get current timestamp
 std::string get_timestamp(const char *format = "%Y-%m-%d %H:%M:%S");
+
+// Split string with separator
+std::vector<std::string> split(const std::string &str, char delimiter);
 
 } // namespace string_utils
--- src/utils/string.cc
+++ src/utils/string.cc
@@ -9,4 +9,19 @@ std::string get_timestamp(const char *format) {
   return fmt::format("{:" + std::string(format) + "}", fmt::localtime(t));
 }
 
+std::vector<std::string> split(const std::string &str, char delimiter) {
+  std::vector<std::string> result;
+  size_t start = 0;
+  size_t end;
+
+  while ((end = str.find(delimiter, start)) != std::string::npos) {
+    result.push_back(str.substr(start, end - start));
+    start = end + 1;
+  }
+
+  // Always push the remaining part, even if empty
+  result.push_back(str.substr(start));
+  return result;
+}
+
 } // namespace string_utils
