21e731507b0320c913b06f51f1805d235acec850
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-14 10:57:09 +0700

    feat(utils): add join and split string utils

--- include/utils/string.hh
+++ include/utils/string.hh
@@ -11,4 +11,10 @@ std::string get_timestamp(const char *format = "%Y-%m-%d %H:%M:%S");
 // Split string with separator
 std::vector<std::string> split(const std::string &str, char delimiter);
 
+// Convert a string to title case
+std::string to_title(const std::string &str);
+
+// Join a vector to string
+std::string join(const std::vector<std::string> &values, const std::string &delimiter);
+
 } // namespace string_utils
--- src/utils/string.cc
+++ src/utils/string.cc
@@ -1,4 +1,5 @@
 #include "utils/string.hh"
+#include <cctype>
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
@@ -22,6 +23,38 @@ std::vector<std::string> split(const std::string &str, char delimiter) {
   // Always push the remaining part, even if empty
   result.push_back(str.substr(start));
   return result;
+}
+
+std::string to_title(const std::string &str) {
+  if (str.empty()) {
+    return str;
+  }
+
+  std::string res;
+  res += std::toupper(str[0]);
+  auto n = str.size();
+
+  for (size_t i = 1; i < n; i++) {
+    res += std::tolower(str[i]);
+  }
+
+  return res;
+}
+
+std::string join(const std::vector<std::string> &values,
+                 const std::string &delimiter) {
+  if (values.empty()) {
+    return "";
+  }
+
+  std::string res = values[0];
+  size_t n = values.size();
+
+  for (size_t i = 1; i < n; i++) {
+    res += delimiter + values[i];
+  }
+
+  return res;
 }
 
 } // namespace string_utils
