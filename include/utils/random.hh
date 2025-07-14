78b797c4bf92dd63f12209f7cee27d8c3b7de21e
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-14 08:01:29 +0700

    perf(random): make random device and mt19937 static

--- include/utils/random.hh
+++ include/utils/random.hh
@@ -6,8 +6,8 @@ namespace random_utils {
 
 // Generate a random number in range [a, b]
 template <typename T> T random_int_range(T a, T b) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
   std::uniform_int_distribution<> distrib(a, b);
   return distrib(gen);
 };
